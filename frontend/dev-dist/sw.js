77b231ca17db89e7a1f8e2f40962b2ea411aff6e
itspksharma <210720100076@cutm.ac.in>
2025-08-26 10:53:12 +0530

    update gitignore

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-cdd9fcb5'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.9dmto18b0cg"
+    "revision": "0.5tetieh9cdo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
